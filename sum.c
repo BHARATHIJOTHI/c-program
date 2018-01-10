bda083884cd902e9e5524419fb4753758bca16f0
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-10 16:56:50 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int k,i,n,a[i],s=0;
+printf("enter the num");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the k value");
+scanf("%d",&k);
+if(i<=k)
+{s=s+a[i];
+}
+printf("sum is%d",s);
+}
