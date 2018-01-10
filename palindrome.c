2aaa472187c2881aabaa790f1cf58cedd1f764cc
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-10 19:14:13 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void  main()
+{
+char  a[10],b[10];
+printf("enter the string");
+scanf("%s",a);
+strcpy(b,a);
+strrev(a);
+i=strcmp(a,b);
+if(i==0)
+{
+printf("palindrome");
+}
+else
+{
+printf("not palindrome");
+}}
