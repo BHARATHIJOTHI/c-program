c534b1e1a852eb7ee02b4f86785c4726a613c1be
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-16 17:17:22 +0530

    Create armstrong or not.c

---
+++ armstrong or not.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void  main()
+{
+int n,s=0,d,a;
+printf("enter the num");
+scanf("%d",&n);
+a=n;
+while(a!=0)
+{
+d=a%10;
+s=s+d*d*d;
+a=a/10;
+}
+if(s==n)
+printf("armstrong num");
+else
+printf("not armstrong num");
+}
