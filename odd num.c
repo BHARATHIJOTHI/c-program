743e5374a68cefbcd188dfd3a926b5316e644e91
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-19 16:01:32 +0530

    Create odd num.c

---
+++ odd num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[100],,i;
+printf("enter the num");
+scanf("%d",&a[100]);
+for(i=0;i<a[100];i++)
+{
+if((i%2)==0)
+printf("the num is not odd"):
+else
+printf("the num is odd");
+}}
