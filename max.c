43dc8468318eab5b3d57de767cc613d8e2bd55b9
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-19 20:36:23 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a[100],i,c=0;
+printf("enter the numbers");
+scanf("%d",&a[100]);
+for(i=0;i<a[100];i++)
+{
+for(j=a[100];j>0;j--)
+{
+strcmp(i,j);
+if(i<j)
+{
+printf("%d",j);
+}}}}
