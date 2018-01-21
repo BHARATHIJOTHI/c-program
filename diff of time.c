b270e0d022cd19543b65ec625bb163db38051598
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-21 11:08:01 +0530

    Create diff of time.c

---
+++ diff of time.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,f;
+printf("enter the time");
+scanf("%d%d",&a,&b);
+printf("enter the minute");
+scanf("%d%d",&c,&d)
+e=a-b;
+f=c,d;
+printf("the diff between time and min is%d:%d",e,f);
+}
