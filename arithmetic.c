ae2fed9ab4892ec29938700e70bf6141c220bf5b
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-21 10:37:02 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,,i,d=0;
+printf("enter the value");
+scanf("%d",&a);
+printf("enter the diff");
+scanf("%d",&b);
+for(i=0;i<a;i++)
+{
+printf("%d",c);
+d+=c;
+a=a+b;
+}printf("%d%d",a,c);
+}
