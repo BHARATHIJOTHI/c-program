5fedd7122d4c14852015af18e20fb85f18fd3e31
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-21 11:22:32 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,c;
+printf("enter the minutes");
+scanf("%d",&a);
+if(a<60)
+printf("the time is 0:%d",&a)
+else
+{
+c=a/60;
+printf("the time is %d",c)
+}}
