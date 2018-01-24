e105b147074c852604bea856f63fe418b5d30fa8
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-24 16:52:43 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define FILENAME "test.txt"
+int main()
+{
+	FILE *fp;
+	char ch;
+	int linesCount=0;
+	fp=fopen(FILENAME,"r");
+	if(fp==NULL)
+	{
+		printf("File \"%s\" does not exist!!!\n",FILENAME);
+		return ();	
+	while((ch=fgetc(fp))!=EOF)
+	{
+		if(ch=='\n')
+			linesCount++;
+	}
+	fclose(fp);
+	printf("Total number of lines are: %d\n",linesCount);	
+	return 0;	
+}
