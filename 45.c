8fd2bf3bd5b0e9af1414eaf94e839a305ec4e94a
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-28 14:47:20 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,j;
+    printf("enter the no");
+    scanf("%d",&a);
+    for(j=1;j<=10;j++)
+    {
+        if(a<=10)
+        {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    }
+}
