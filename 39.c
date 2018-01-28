ac532e6393b6d8c752355931996df520773faeae
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-01-28 13:50:34 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a[20]={1,7,8,9,5,24,67,89,18,100},i,max;
+    max=a[0];
+    for(i=0;i<=10;i++)
+    {
+    
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+        printf("the max no is %d",a[i]);
+        
+}
