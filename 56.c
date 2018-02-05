c7c1fa4d97e66c3b471c5e4dce7e9e158beb8055
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-02-05 15:48:11 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[50];
+printf("enter the string");
+scanf("%s",&s);
+if((s==(a>>z&&a<<z))&&(s==(A>>Z&&A<<Z))&&(s=='\0'||s=='1',||s=='2'||s=='3'||s=='4'||s=='5'||s=='6'||s=='7'||s=='8'||s=='9'))
+{
+printf("it contains alphabet and numeric");
+}
+else
+{
+printf("it does not contain alphabet and numeric");
+}}
