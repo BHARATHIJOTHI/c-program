22aec04abebda91ea4eaf006c9bd2394a9f2816e
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-02-06 19:53:07 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void  main()
+{
+char  a[100],i,n;
+printf("enter the limit");
+scanf("%d",&n);
+printf("enter the string");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+if(a[i]=='\0'&&a[i]=='1')
+{
+printf("it  is  a binary  representation");
+}
+else
+{
+printf("it  is  not a binary  representation");
+}}
