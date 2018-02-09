7f4b73c6c11962e97a660e5447ce2c276195fa69
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-02-09 16:35:28 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,c=0;
+	printf("enter the num");
+	scanf("%d",&n);
+	for(i=n;i<200;i++)
+	{
+	if(i%2==0)
+	{
+		printf("%d",i);
+		break;
+	}
+		
+	
+	
+		
+	}
+}
