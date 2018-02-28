7c4ac7c74a5b83c35713c7acc8e79f88c7b0d245
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-02-28 15:46:30 +0530

    Create 86.c

---
+++ 86.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[50];
+	int len,i,j,c=0;
+	scanf("%d",&a);
+	len=strlen(a);
+	for(i=0;i<len;i++)
+	{
+		for(j=0;j<len;j++)
+		{
+			if(a[i]==a[j])
+			{
+				c++;
+			}
+		}
+	}
+	if(c==0)
+	{
+		printf("it is isogram");
+	}
+	else
+	{
+		printf("it is not a isogram");
+	}
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
