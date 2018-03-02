9eae8ffd7d7977614c79b73dadbe834cea282131
BHARATHIJOTHI <35127778+BHARATHIJOTHI@users.noreply.github.com>
2018-03-02 16:28:55 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+    	if((n%i)==0)
+    	{
+    		c++;
+    	}
+  
+    }
+    if(c==0)
+    {
+    printf("not composite");
+    
+    }
+    else
+    {
+    	printf("composite");
+    }
+	return 0;
+}
